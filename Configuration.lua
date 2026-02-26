c0b6933097b33d75d3b965e1feee8c2dccb4c41f
Gregory909 <124597526+Gregory909@users.noreply.github.com>
2026-02-26 22:56:35 +0500

    script_change: warning add by gregory909

--- Configuration.lua
+++ Configuration.lua
@@ -1,7 +1,9 @@
 local API = {};
 local HardBans = {"1293792120","1993687031","604430770","1222680","6225378520","7166779222","10035014576"};
 local IPBans = {};
-local Warnings = {};
+local Warnings = {
+    ["3441475358"] = {Reason = "N words in chat", Moderator = "gregory909"},
+};
 function API:GetHardBans()
 	return HardBans;
 end;
