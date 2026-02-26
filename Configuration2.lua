ea3db524a8bb7691ca39b9325c99fb2029a0241d
Gregory909 <124597526+Gregory909@users.noreply.github.com>
2026-02-26 16:58:03 +0300

    Create Configuration2.lua

---
+++ Configuration2.lua
@@ -0,0 +1,19 @@
+local API = {};
+
+local ChatModeration = {};
+
+function API:GetChatModeration()
+  return ChatModeration;
+end;
+
+function API:GetUserState(userId)
+  return ChatModeration[tostring(userId)] or {};
+end;
+
+function API:IsMuted(userId)
+  local state = API:GetUserState(userId)
+  local mutedUntil = tonumber(state.MutedUntil) or 0
+  return mutedUntil > os.time(), mutedUntil
+end;
+
+return API;
