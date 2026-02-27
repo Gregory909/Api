982c36dcb539b1b3e42df5e9d3bb1250d53497bf
Gregory909 <124597526+Gregory909@users.noreply.github.com>
2026-02-27 07:45:50 +0500

    Updated File!

--- Configuration2.lua
+++ Configuration2.lua
@@ -1,19 +1,26 @@
 local API = {};
 
-local ChatModeration = {};
+local ChatModeration = {
+    ["10133766492"] = {
+        LastReason = "blacklist bypass attempt",
+        LastUpdated = 1772160296,
+        MutedUntil = 1772160596,
+        Strikes = 1
+    }
+};
 
 function API:GetChatModeration()
-  return ChatModeration;
+    return ChatModeration;
 end;
 
 function API:GetUserState(userId)
-  return ChatModeration[tostring(userId)] or {};
+    return ChatModeration[tostring(userId)] or {};
 end;
 
 function API:IsMuted(userId)
-  local state = API:GetUserState(userId)
-  local mutedUntil = tonumber(state.MutedUntil) or 0
-  return mutedUntil > os.time(), mutedUntil
+    local state = API:GetUserState(userId)
+    local mutedUntil = tonumber(state.MutedUntil) or 0
+    return mutedUntil > os.time(), mutedUntil
 end;
 
 return API;
