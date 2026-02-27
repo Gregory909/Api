2526f30eb58324857e2af66431fe02c7802403b8
Gregory909 <124597526+Gregory909@users.noreply.github.com>
2026-02-27 17:13:28 +0500

    Updated File!

--- Configuration2.lua
+++ Configuration2.lua
@@ -1,6 +1,13 @@
 local API = {};
 
-local ChatModeration = {};
+local ChatModeration = {
+    ["10476349025"] = {
+        LastReason = "cooldown bypass",
+        LastUpdated = 1772194408,
+        MutedUntil = 1772194708,
+        Strikes = 1
+    }
+};
 
 function API:GetChatModeration()
     return ChatModeration;
