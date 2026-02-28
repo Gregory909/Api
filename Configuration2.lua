2901083909809472fbcfd043dca9b24cafd197b4
Gregory909 <124597526+Gregory909@users.noreply.github.com>
2026-03-01 04:44:07 +0500

    Updated File!

--- Configuration2.lua
+++ Configuration2.lua
@@ -1,6 +1,12 @@
 local API = {};
 
 local ChatModeration = {
+    ["4009693961"] = {
+        LastReason = "webhook rejected message",
+        LastUpdated = 1772322242,
+        MutedUntil = 1772322542,
+        Strikes = 1
+    },
     ["9432475747"] = {
         LastReason = "blacklist bypass attempt",
         LastUpdated = 1772321194,
