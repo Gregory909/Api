d473d0b15c01423aa0405ee487edffc5c3661cdc
Gregory909 <124597526+Gregory909@users.noreply.github.com>
2026-03-01 18:45:11 +0500

    Updated File!

--- Configuration2.lua
+++ Configuration2.lua
@@ -1,25 +1,6 @@
 local API = {};
 
-local ChatModeration = {
-    ["4009693961"] = {
-        LastReason = "webhook rejected message",
-        LastUpdated = 1772322242,
-        MutedUntil = 1772322542,
-        Strikes = 1
-    },
-    ["8775533108"] = {
-        LastReason = "strike decay",
-        LastUpdated = 1772358254,
-        MutedUntil = 1772356261,
-        Strikes = 0
-    },
-    ["9432475747"] = {
-        LastReason = "blacklist bypass attempt",
-        LastUpdated = 1772321194,
-        MutedUntil = 1772321494,
-        Strikes = 1
-    }
-};
+local ChatModeration = {};
 
 function API:GetChatModeration()
     return ChatModeration;
